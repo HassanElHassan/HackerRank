d92bec60488a107bc12f2bb169c1377190302e53
HassanElHassan <39101530+HassanElHassan@users.noreply.github.com>
2020-06-30 21:02:56 +0200

    Update Day 0: Weighted Mean.R

--- 10 Days of Statistics/Day 0: Weighted Mean.R
+++ 10 Days of Statistics/Day 0: Weighted Mean.R
@@ -1,9 +1,12 @@
 # challenge: https://www.hackerrank.com/challenges/s10-weighted-mean/problem
 
 con <- file("stdin", open = "r")
+
 dataset <- readLines(con)
+
 ls_grade <- unlist(strsplit(dataset[2], split=" "))
 ls_grade <- as.numeric(ls_grade)
+
 ls_weight <- unlist(strsplit(dataset[3], split=" "))
 ls_weight <- as.numeric(ls_weight)
 
