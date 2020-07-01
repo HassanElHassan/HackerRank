400fb14c88293f25b85f77f10419307855e067b9
HassanElHassan <39101530+HassanElHassan@users.noreply.github.com>
2020-07-01 02:52:22 +0200

    Create Day 1: Standard Deviation.R

---
+++ 10 Days of Statistics/Day 1: Standard Deviation.R
@@ -0,0 +1,27 @@
+# https://www.hackerrank.com/challenges/s10-standard-deviation/problem
+
+# the build in sd() function is for sample not population
+# an option using this sd(): sqrt((n-1)/n) * sd(x)
+
+# function to calculate the population sd
+popsd <- function(v_val){
+    # average the elements in the vector
+    v_val_avg <- mean(v_val)
+    # squared distance from the mean
+    v_val_dist <- (v_val-v_val_avg)^2
+    # average squared distance from the mean
+    v_val_dist_avg <- sum(v_val_dist) / length(v_val)
+    # SD by square root of average squared distance from the mean
+    sqrt(v_val_dist_avg)
+}   
+
+con <- file("stdin", open = "r")
+
+dataset <- readLines(con)
+
+v_val <- unlist(strsplit(dataset[2], split=" "))
+v_val <- as.numeric(v_val)
+
+cat(round(popsd(v_val),digits=1))
+
+close(con)
