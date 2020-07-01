e2fe212eece480eb6bd0f4963c2ff4e036cc6e7a
HassanElHassan <39101530+HassanElHassan@users.noreply.github.com>
2020-07-01 02:29:59 +0200

    Update Day 0: Mean, Median, and Mode.R

--- 10 Days of Statistics/Day 0: Mean, Median, and Mode.R
+++ 10 Days of Statistics/Day 0: Mean, Median, and Mode.R
@@ -1,17 +1,17 @@
 # challenge: https://www.hackerrank.com/challenges/s10-basic-statistics/problem
 
+mode <- function(v) {
+    v_val_tab <- table(v_values)
+    v_val_tab <- sort(v_val_tab, decreasing=TRUE)
+    return(as.numeric(names(v_val_tab[1])))
+}
+
 con <- file("stdin", open = "r")
 
 dataset <- readLines(con)
 
 v_values <- unlist(strsplit(dataset[2], split=" "))
 v_values <- as.numeric(v_values)
-
-mode <- function(v) {
-    v_val_tab <- table(v_values)
-    v_val_tab <- sort(v_val_tab, decreasing=TRUE)
-    return(as.numeric(names(v_val_tab[1])))
-}
 
 cat(round(mean(v_values), digits=1))
 cat("\n")
