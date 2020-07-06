4792b827ce805ab7ffba56e5192fe8cb8a85a112
HassanElHassan <39101530+HassanElHassan@users.noreply.github.com>
2020-07-06 23:22:23 +0200

    Update Day 0: Mean, Median, and Mode.py

--- 10 Days of Statistics/Day 0: Mean, Median, and Mode.py
+++ 10 Days of Statistics/Day 0: Mean, Median, and Mode.py
@@ -1,6 +1,16 @@
 # challenge: https://www.hackerrank.com/challenges/s10-basic-statistics/problem
 # all 3 stats and more can be computed using the statistics library
 
+"""
+Objective
+In this challenge, we practice calculating the mean, median, and mode. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Given an array, X, of N integers, calculate and print the respective mean, median, and mode on separate lines. If your array contains more than one modal value, choose the numerically smallest one.
+
+Note: Other than the modal value (which will always be an integer), your answers should be in decimal form, rounded to a scale of 1 decimal place.
+"""
+
 from collections import Counter
 
 # number of elements in list computed using len()
