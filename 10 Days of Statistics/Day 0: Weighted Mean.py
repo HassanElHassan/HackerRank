8fc436eddb99f8821646d7a9380aaae517d7a3a6
HassanElHassan <39101530+HassanElHassan@users.noreply.github.com>
2020-07-06 23:32:46 +0200

    Update Day 0: Weighted Mean.py

--- 10 Days of Statistics/Day 0: Weighted Mean.py
+++ 10 Days of Statistics/Day 0: Weighted Mean.py
@@ -1,4 +1,15 @@
 # challenge: https://www.hackerrank.com/challenges/s10-weighted-mean/problem
+
+"""
+Objective
+In the previous challenge, we calculated a mean. In this challenge, we practice calculating a weighted mean.
+Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Given an array, X, of N integers and an array, W, representing the respective weights of X's elements, calculate and print the weighted mean of X's elements.
+Your answer should be rounded to a scale of 1 decimal place.
+"""
+
 # numpy not allowed for this challenge
 # import numpy as np gives ModuleNotFoundError: No module named 'numpy'
 
