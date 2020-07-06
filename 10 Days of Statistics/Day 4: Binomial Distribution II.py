6e8890ebaaa5ab0e8eb9aea5827c1957f9f731d6
HassanElHassan <39101530+HassanElHassan@users.noreply.github.com>
2020-07-06 23:43:44 +0200

    Update Day 4: Binomial Distribution II.py

--- 10 Days of Statistics/Day 4: Binomial Distribution II.py
+++ 10 Days of Statistics/Day 4: Binomial Distribution II.py
@@ -1,5 +1,17 @@
 # https://www.hackerrank.com/challenges/s10-binomial-distribution-2/problem
 
+"""
+Objective
+In this challenge, we go further with binomial distributions.
+
+Task
+A manufacturer of metal pistons finds that, on average, 12% of the pistons they manufacture are rejected because they are incorrectly sized.
+What is the probability that a batch of 10 pistons will contain:
+
+No more than 2 rejects?
+At least 2 rejects?
+"""
+
 # from itertools import combinations
 from math import factorial as fact
 
