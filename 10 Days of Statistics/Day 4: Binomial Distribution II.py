7c11603a4d0a3c377032940bea9de29a5a38038d
HassanElHassan <39101530+HassanElHassan@users.noreply.github.com>
2020-07-07 00:09:17 +0200

    Update Day 4: Binomial Distribution II.py

--- 10 Days of Statistics/Day 4: Binomial Distribution II.py
+++ 10 Days of Statistics/Day 4: Binomial Distribution II.py
@@ -7,9 +7,8 @@ In this challenge, we go further with binomial distributions.
 Task
 A manufacturer of metal pistons finds that, on average, 12% of the pistons they manufacture are rejected because they are incorrectly sized.
 What is the probability that a batch of 10 pistons will contain:
-
-No more than 2 rejects?
-At least 2 rejects?
+1. No more than 2 rejects?
+2. At least 2 rejects?
 """
 
 # from itertools import combinations
