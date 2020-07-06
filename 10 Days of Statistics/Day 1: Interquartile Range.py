b13c0b9745f6c466e1e001a470b45fb52728c6b6
HassanElHassan <39101530+HassanElHassan@users.noreply.github.com>
2020-07-06 23:35:52 +0200

    Update Day 1: Interquartile Range.py

--- 10 Days of Statistics/Day 1: Interquartile Range.py
+++ 10 Days of Statistics/Day 1: Interquartile Range.py
@@ -1,5 +1,17 @@
 # https://www.hackerrank.com/challenges/s10-interquartile-range/problem
 
+"""
+Objective
+In this challenge, we practice calculating the interquartile range. We recommend you complete the Quartiles challenge before attempting this problem.
+
+Task
+The interquartile range of an array is the difference between its first (Q1) and third (Q3) quartiles (i.e., Q3-Q1).
+Given an array, X, of n integers and an array, F, representing the respective frequencies of X's elements, construct a data set, S, where each xi occurs at frequency fi.
+Then calculate and print S's interquartile range, rounded to a scale of 1 decimal place.
+
+Tip: Be careful to not use integer division when averaging the middle two elements for a data set with an even number of elements, and be sure to not include the median in your upper and lower data sets.
+"""
+
 # quartiles calculated based on the first method described in the Wikipedia:
 # https://en.wikipedia.org/wiki/Quartile#Method_1
 
