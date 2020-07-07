5fb766503ce951dc70688a5a610cc431dc0510f1
HassanElHassan <39101530+HassanElHassan@users.noreply.github.com>
2020-07-07 12:40:34 +0200

    Update Day 0: Weighted Mean.py

--- 10 Days of Statistics/Day 0: Weighted Mean.py
+++ 10 Days of Statistics/Day 0: Weighted Mean.py
@@ -12,23 +12,12 @@ Your answer should be rounded to a scale of 1 decimal place.
 # numpy not allowed for this challenge
 # import numpy as np gives ModuleNotFoundError: No module named 'numpy'
 
-# number of elements in list computed using len()
-# following variable only needed for the challenge to work but not for the script
-n=input()
-
-ls_grade = list(map(int, input().rstrip().split()))
-
-ls_weight = list(map(int, input().rstrip().split()))
-
 # the following is based on numpy that cannot be used
 """
 def weighted_mean(ls_grade, ls_weight):
     mult = np.multiply(ls_grade, ls_weight)
     mult_sum = mult.sum()
     weight_sum = sum(ls_weight)
-    print(mult)
-    print(mult_sum)
-    print(weight_sum)
     return round(mult_sum/weight_sum,1)
 
 weighted_mean(ls_grade, ls_weight)
@@ -39,5 +28,13 @@ def weighted_mean(ls_grade, ls_weight):
     mult_sum = sum(mult)
     weight_sum = sum(ls_weight)
     return round(mult_sum/weight_sum,1)
+
+# number of elements in list computed using len()
+# following variable only needed for the challenge to work but not for the script
+n=input()
+
+ls_grade = list(map(int, input().rstrip().split()))
+
+ls_weight = list(map(int, input().rstrip().split()))
 
 print(weighted_mean(ls_grade, ls_weight))
