b219942131fcc1992a3deb0c87674e6d4c2f5d30
HassanElHassan <39101530+HassanElHassan@users.noreply.github.com>
2020-07-18 11:35:22 +0200

    Update Day 0: Hello, World.java

--- 30 Days of Code/Day 0: Hello, World.java
+++ 30 Days of Code/Day 0: Hello, World.java
@@ -14,20 +14,20 @@ To complete this challenge, you must save a line of input from stdin to a variab
 
 public class Solution {
 	public static void main(String[] args) {
-        // Create a Scanner object to read input from stdin.
+        	// Create a Scanner object to read input from stdin.
 		Scanner scan = new Scanner(System.in); 
 		
-		// Read a full line of input from stdin and save it to our variable, inputString.
+		// Read a full line of input from stdin and save it to the variable inputString.
 		String inputString = scan.nextLine(); 
 
 		// Close the scanner object, because we've finished reading 
-        // all of the input from stdin needed for this challenge.
+        	// all of the input from stdin needed for this challenge.
 		scan.close(); 
       
 		// Print a string literal saying "Hello, World." to stdout.
 		System.out.println("Hello, World.");
       
-	    // TODO: Write a line of code here that prints the contents of inputString to stdout.
-        System.out.println(inputString);
+	    	// TODO: Write a line of code here that prints the contents of inputString 
+        	System.out.println(inputString);
 	}
 }
